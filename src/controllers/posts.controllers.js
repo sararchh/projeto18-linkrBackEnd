a0167e9a4ab5ad00977dd8c64670e579ba1d0e48
DanielIbiapina <daniel18.ibiapinabarros@gmail.com>
2023-01-04 17:24:12 -0300

    feat: rota post

--- src/controllers/posts.controllers.js
+++ src/controllers/posts.controllers.js
@@ -1,12 +1,31 @@
 import connectDB from "../database/database.js"
 
+const db = await connectDB();
 
 export async function getPosts(req, res){
 
 try{
-    const posts = await connectDB.query('SELECT * FROM posts;')
-    res.send(posts.rows[0])
+    const posts = await db.query('SELECT * FROM posts ORDER BY "createdAt" DESC LIMIT 20;')
+    res.send(posts.rows)
 }catch (err) {
     res.status(500).send(err.message);
 }
 }
+
+
+export async function createPost(req, res){
+    const {url, content} = req.body
+    const userId = 1
+    /*const { authorization } = req.headers;
+      const token = authorization?.replace("Bearer ", "");*/
+
+try{
+    
+    await db.query('INSERT INTO posts (url, content, "userId") VALUES ($1, $2, $3);', [url, content, userId])
+    res.send('post criado')
+}catch (err) {
+    res.status(500).send(err.message);
+}
+}
+
+
--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -1,9 +1,10 @@
 import {Router} from "express";
-import { getPosts } from "../controllers/posts.controllers.js";
+import { createPost, getPosts } from "../controllers/posts.controllers.js";
 
 const router = Router();
 
 router.get("/timeline", getPosts)
+router.post("/timeline", createPost)
 
 
 export default router;

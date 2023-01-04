fba1f4dd83aa2660e54bb957d265b546224d1521
Luiz <felipedevluiz@gmail.com>
2023-01-04 19:54:35 -0300

    fix: bugs at signup

--- src/repositories/userRepositories.js
+++ src/repositories/userRepositories.js
@@ -1,19 +1,20 @@
-import db from "../database/database.js";
+import connectDB from "../database/database.js";
 import bcrypt from "bcrypt";
 
 async function getByEmail(email) {
-    return db.query(`SELECT * FROM users WHERE email=$1;`, [email.toLowerCase()]);
+    const db = await connectDB(`SELECT * FROM users WHERE email=$1;`, [email.toLowerCase()]);
+    return db;
 };
 
 async function insertUser(data) {
     const { email, password, username, pictureUrl } = data;
     const passwordHash = bcrypt.hashSync(password, 12);
     try {
-        await db.query(`INSERT INTO users (email, password, username, pictureUrl) VALUES ($1, $2, $3, $4);`, [email.toLowerCase(), passwordHash, username, pictureUrl]);
+        const db = await connectDB(`INSERT INTO users (email, password, username, pictureUrl) VALUES ($1, $2, $3, $4);`, [email.toLowerCase(), passwordHash, username, pictureUrl]);
     } catch (err) {
         console.log(err);
     }
-}
+};
 
 const userRepository = {
     getByEmail,

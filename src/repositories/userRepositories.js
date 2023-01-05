18a2ff8aa547ec430c157abbd7187609a63c6adc
Luiz <felipedevluiz@gmail.com>
2023-01-05 10:01:25 -0300

    refact: signup

--- src/repositories/userRepositories.js
+++ src/repositories/userRepositories.js
@@ -1,16 +1,18 @@
 import connectDB from "../database/database.js";
 import bcrypt from "bcrypt";
 
+const db = await connectDB();
+
 async function getByEmail(email) {
-    const db = await connectDB(`SELECT * FROM users WHERE email=$1;`, [email.toLowerCase()]);
-    return db;
+    const userExist = await db.query(`SELECT * FROM users WHERE email=$1;`, [email.toLowerCase()]);
+    return userExist;
 };
 
 async function insertUser(data) {
     const { email, password, username, pictureUrl } = data;
     const passwordHash = bcrypt.hashSync(password, 12);
     try {
-        const db = await connectDB(`INSERT INTO users (email, password, username, pictureUrl) VALUES ($1, $2, $3, $4);`, [email.toLowerCase(), passwordHash, username, pictureUrl]);
+        await db.query(`INSERT INTO users (email, password, username, pictureUrl) VALUES ($1, $2, $3, $4);`, [email.toLowerCase(), passwordHash, username, pictureUrl]);
     } catch (err) {
         console.log(err);
     }

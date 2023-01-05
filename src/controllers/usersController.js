869f57334eabb39dd553805dad5100d61cd34e25
Sara Rocha de Oliveira <sararocha878@gmail.com>
2023-01-04 22:09:11 -0300

    feat/searchusers

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -10,7 +10,7 @@ export default {
       const { name } = req.params;
       const selectedUsers = await db.query(`SELECT * FROM users WHERE username LIKE '%' || $1 || '%'`, [name]);
 
-      return res.status(200).send({ data: selectedUsers.rows });
+      return res.status(200).send( selectedUsers.rows );
     } catch (error) {
       console.log('error', error);
       return res.sendStatus(404);

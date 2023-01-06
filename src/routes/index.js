4a44ef79b9ad398c9acbe1983e8b91bf74a65c39
ConstantinoRafael <rafael.constantino89@gmail.com>
2023-01-06 08:26:55 -0300

    fix: add routes use post router

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@ import { Router } from "express";
 import userRouter from "./userRouter.js";
 import authRouter from "../routes/authRouter.js"
 import likeRouter from "./likeRouter.js";
+import postRouter from "./posts.routes.js"
 
 const routes = Router();
 
 routes.use(userRouter);
 routes.use(authRouter);
 routes.use(likeRouter);
+routes.use(postRouter);
 
 routes.get('/', (req, res, next) => {
   return res.status(200).json({ message: "Servidor em operacao" })

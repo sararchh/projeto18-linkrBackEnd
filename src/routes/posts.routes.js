a5ab24849b68d4050110369c4574eeee9c86d78f
DanielIbiapina <daniel18.ibiapinabarros@gmail.com>
2023-01-08 16:15:41 -0300

    feat: funcionalidade de editar/ checkjwt consertdado

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -1,11 +1,12 @@
 import {Router} from "express";
-import { createPost, deletePost, getPosts } from "../controllers/posts.controllers.js";
+import { createPost, deletePost, editPost, getPosts } from "../controllers/posts.controllers.js";
 import {checkjwt} from "../middlewares/checkjwt.js"
 const router = Router();
 
-router.get("/timeline",  getPosts)
-router.post("/timeline", createPost)
-router.delete("/timeline/:id", deletePost)
+router.get("/timeline", [checkjwt], getPosts)
+router.post("/timeline", [checkjwt], createPost)
+router.delete("/timeline/:id", [checkjwt], deletePost)
+router.post("/timeline/:id", [checkjwt], editPost)
 
 
 export default router;

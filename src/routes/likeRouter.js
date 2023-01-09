0546f78a8f305b11f84c0527c940c884098226ea
ConstantinoRafael <rafael.constantino89@gmail.com>
2023-01-09 16:36:51 -0300

    fix: like route

--- src/routes/likeRouter.js
+++ src/routes/likeRouter.js
@@ -1,11 +1,15 @@
 import { Router } from "express";
-import { getUsersLikesByPostId, likePost, unlikePost } from "../controllers/likesController.js";
-
+import {
+  getUsersLikesByPostId,
+  likePost,
+  unlikePost,
+} from "../controllers/likesController.js";
+import { checkjwt } from "../middlewares/checkjwt.js";
 
 const likeRouter = Router();
 
-likeRouter.post("/timeline/:id/like", likePost); //falta a autenticação do token
-likeRouter.post("/timeline/:id/unlike", unlikePost);
+likeRouter.post("/timeline/:id/like", [checkjwt], likePost); //falta a autenticação do token
+likeRouter.post("/timeline/:id/unlike", [checkjwt], unlikePost);
 likeRouter.get("/likes/:id", getUsersLikesByPostId);
 
 export default likeRouter;

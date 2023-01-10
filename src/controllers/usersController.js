5f83ffe5195878b9519e1c0aa7febe6e270af6fe
Luiz <felipedevluiz@gmail.com>
2023-01-09 22:03:08 -0300

    feat: user timelime

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -10,9 +10,32 @@ export default {
       const { name } = req.params;
       const selectedUsers = await db.query(`SELECT * FROM users WHERE username LIKE '%' || $1 || '%'`, [name]);
 
-      return res.status(200).send( selectedUsers.rows );
+      return res.status(200).send(selectedUsers.rows);
     } catch (error) {
       return res.sendStatus(404);
     }
+  }
+}
+
+export async function findUserById(req, res) {
+  try {
+    const { id } = req.params;
+    const userInfo = await db.query(`SELECT * FROM users WHERE id $1;`, [id]);
+    const userPosts = await db.query(`SELECT * FROM posts WHERE userId $1;`, [id]);
+
+    if (userPosts.length === 0) {
+      userPosts = "User has no posts!"
+    }
+
+    const body = {
+      username: userInfo.rows[0].username,
+      pictureUrl: userInfo.rows[0].pictureUrl,
+      userPosts: [userPosts.rows]
+    };
+
+    return res.status(200).send(body);
+
+  } catch (err) {
+    return res.sendStatus(404);
   }
 }
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,9 +1,12 @@
 import express, { Router } from 'express';
 import usersController from "../controllers/usersController.js";
+import { findUserById } from "../controllers/usersController.js";
+import { checkjwt } from '../middlewares/checkjwt.js';
 import { userSearch } from "../middlewares/userSearch.js";
 
 const userRouter = Router();
 
 userRouter.get("/users/:name", [userSearch], usersController.findAll);
+userRouter.get("/users/:id", [checkjwt], findUserById);
 
 export default userRouter;

8b592093ad95558983fab82df1ec4405d82bf7b4
Sara Rocha de Oliveira <sararocha878@gmail.com>
2023-01-10 21:31:23 -0300

    feat/findUserById

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -23,18 +23,32 @@ export async function findUserById(req, res) {
 
     const userInfo = await db.query(`SELECT * FROM users WHERE id = $1;`, [id]);
     const userPosts = await db.query(`SELECT * FROM posts WHERE "userId" = $1;`, [id]);
-    const likes = await db.query(`SELECT * FROM likes WHERE "userId" = $1;`, [id]);
+    const likes = await db.query(`SELECT * FROM likes JOIN users ON likes."userId" = users.id`);
+
+    const posts = await db.query(
+      `SELECT posts.*,posts.id AS "postId", users.* FROM posts JOIN users ON posts."userId" = users.id 
+      WHERE  posts."userId" = $1
+      ORDER BY posts."createdAt" DESC LIMIT 20;`, [id]
+    );
+
+
+    //para pegar os usuários que curtiram cada post
+    const usersLiked = await db.query(
+      'SELECT users."username", likes."postId" FROM users JOIN likes ON users.id = likes."userId";'
+    );
 
 
     if (userPosts.length === 0) {
       userPosts = "User has no posts!"
     }
 
+
     const body = {
       username: userInfo.rows[0].username,
       pictureUrl: userInfo.rows[0].pictureUrl,
-      userPosts: [userPosts.rows],
-      likes: [likes.rows]
+      user: userInfo.rows[0],
+      likes: usersLiked.rows,
+      posts: posts.rows,
     };
 
     return res.status(200).send(body);
